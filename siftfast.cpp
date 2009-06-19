6524870dac9c8df4627dff4037803a61c3bd85a0
zerofrog <zerofrog@f95a89db-1ff5-47d4-b55f-6d1eaed53255>
2009-06-19 01:16:09 +0000

    added python bindings (thanks to Chris Murphy for ideas)

--- siftfast.cpp
+++ siftfast.cpp
@@ -1,5 +1,5 @@
 // exact C++ implementation of lowe's sift program
-// author: zerofrog(@gmail.com), Sep 2008
+// Copyright (C) zerofrog(@gmail.com), 2008-2009
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
